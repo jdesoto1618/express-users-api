1a6b50898d48f1918bad37529b36b96f5e976835
Joe DeSoto <modulusman@gmail.com>
2022-12-07 12:12:38 -0800

    Ability to search for added users via user id is now possible.

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,12 @@ router.get("/", (req, res) => {
   res.send(users);
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
 router.post("/", (req, res) => {
   const newUser = req.body;
   users.push({ id: uuidv4(), ...newUser });
