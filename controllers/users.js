0d837a845a850c560ccd92b0481a923b461574db
Joe DeSoto <modulusman@gmail.com>
2022-12-07 12:52:12 -0800

    Extracts the fucntions and logic away from routes into a controllers folder. The functions are imported and used within the routes.

---
+++ controllers/users.js
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from "uuid";
+
+let users = [];
+
+export const getUsers = (req, res) => {
+  res.send(users);
+};
+
+export const getUserById = (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+};
+
+export const createUser = (req, res) => {
+  const newUser = req.body;
+  users.push({ id: uuidv4(), ...newUser });
+  res.send(`User ${newUser.firstName} was successfully added!`);
+};
+
+export const patchUser = (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, age } = req.body;
+  const userToUpdate = users.find((user) => user.id === id);
+
+  if (firstName) {
+    userToUpdate.firstName = firstName;
+  }
+  if (lastName) {
+    userToUpdate.lastName = lastName;
+  }
+  if (email) {
+    userToUpdate.email = email;
+  }
+  if (age) {
+    userToUpdate.age = age;
+  }
+
+  res.send(`User with the id ${id} has been updated!`);
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => {
+    user.id !== id;
+  });
+
+  res.send(`User with the id ${id} deleted successfully.`);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,21 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  patchUser,
+} from "../controllers/users";
 const router = express.Router();
 
-let users = [];
+router.get("/", getUsers);
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/:id", getUserById);
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  const foundUser = users.find((user) => user.id === id);
-  res.send(foundUser);
-});
+router.post("/", createUser);
 
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
+router.delete("/:id", deleteUser);
 
-  users = users.filter((user) => {
-    user.id !== id;
-  });
-
-  res.send(`User with the id ${id} deleted successfully.`);
-});
-
-router.post("/", (req, res) => {
-  const newUser = req.body;
-  users.push({ id: uuidv4(), ...newUser });
-  res.send(`User ${newUser.firstName} was successfully added!`);
-});
-
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, email, age } = req.body;
-  const userToUpdate = users.find((user) => user.id === id);
-
-  if (firstName) {
-    userToUpdate.firstName = firstName;
-  }
-  if (lastName) {
-    userToUpdate.lastName = lastName;
-  }
-  if (email) {
-    userToUpdate.email = email;
-  }
-  if (age) {
-    userToUpdate.age = age;
-  }
-
-  res.send(`User with the id ${id} has been updated!`);
-});
+router.patch("/:id", patchUser);
 
 export default router;
