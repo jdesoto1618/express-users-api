7e56cef4197bfbf4994639e36ffe760c0dc3b7ac
Joe DeSoto <modulusman@gmail.com>
2022-12-07 12:20:42 -0800

    Adds delete request functionality to the API.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get("/", (req, res) => {
   res.send(users);
@@ -12,6 +12,16 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => {
+    user.id !== id;
+  });
+
+  res.send(`User with the id ${id} deleted successfully.`);
 });
 
 router.post("/", (req, res) => {
