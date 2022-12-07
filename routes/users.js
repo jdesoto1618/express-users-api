296c1b51f3f75ce76c80a00e055c21adbfcb4a2f
Joe DeSoto <modulusman@gmail.com>
2022-12-07 11:16:31 -0800

    Adds two users to the users route. These will show in the terminal when a get request is made to the users route.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 import express from "express";
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Johnny",
+    lastName: "Doe",
+    Email: "johnd@email.com",
+    Age: "25",
+  },
+  {
+    firstName: "Jane",
+    lastName: "Does",
+    Email: "jane@email.com",
+    Age: "22",
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("You reached the users route!");
+  console.log(users);
+  res.send("Users route!");
 });
 
 export default router;
